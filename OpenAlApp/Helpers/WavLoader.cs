b3ab1c84221022e335812ace37ae5fee5f49f3df
Kreso <kreso@dump.hr>
2018-09-16 16:57:29 +0200

    Implemented sound playing, looping and gain options. Added source and listener position and velocity control functionalities

---
+++ OpenAlApp/Helpers/WavLoader.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Audio.OpenAL;
+
+namespace OpenAlApp.Helpers
+{
+    public class WavLoader
+    {
+        public WavLoader(Stream stream)
+        {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
+
+            using (var reader = new BinaryReader(stream))
+            {
+                // RIFF header
+                var signature = new string(reader.ReadChars(4));
+                if (signature != "RIFF")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                reader.ReadInt32(); // Skip Riff chunk size in header
+
+                var format = new string(reader.ReadChars(4));
+                if (format != "WAVE")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                // WAVE header
+                var formatSignature = new string(reader.ReadChars(4));
+                if (formatSignature != "fmt ")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                reader.ReadInt32(); // Skip format chunk size
+                reader.ReadInt16(); // Skip audio format
+                Channels = reader.ReadInt16();
+                Rate = reader.ReadInt32();
+                reader.ReadInt32(); // Skip byte rate
+                reader.ReadInt16(); // Skip block align
+                Bits = reader.ReadInt16();
+
+                var dataSignature = new string(reader.ReadChars(4));
+                if (dataSignature != "data")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                reader.ReadInt32(); // Skip data chunk size
+
+                Data = reader.ReadBytes((int)reader.BaseStream.Length);
+                Format = GetSoundFormat(Channels, Bits);
+            }
+        }
+        public int Channels { get; set; }
+        public int Bits { get; set; }
+        public int Rate { get; set; }
+        public byte[] Data { get; set; }
+        public ALFormat Format { get; set; }
+
+        private static ALFormat GetSoundFormat(int channels, int bits)
+        {
+            switch (channels)
+            {
+                case 1: return bits == 8 ? ALFormat.Mono8 : ALFormat.Mono16;
+                case 2: return bits == 8 ? ALFormat.Stereo8 : ALFormat.Stereo16;
+                default: throw new NotSupportedException("The specified sound format is not supported.");
+            }
+        }
+    }
+}
---
+++ OpenAlApp/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using OpenAlApp.Helpers;
+using OpenTK;
+using OpenTK.Audio;
+using OpenTK.Audio.OpenAL;
+
+namespace OpenAlApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _context = new AudioContext();
+
+            var soundFile = new WavLoader(File.Open("Samples/blip.wav", FileMode.Open));
+
+            _buffer = AL.GenBuffer();
+            AL.BufferData(_buffer, soundFile.Format, soundFile.Data, soundFile.Data.Length, soundFile.Rate);
+
+            _source = AL.GenSource();
+            AL.Source(_source, ALSourcei.Buffer, _buffer);
+        }
+        private readonly AudioContext _context;
+        private readonly int _buffer;
+        private readonly int _source;
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {        
+            AL.SourcePlay(_source);
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            AL.Source(_source, ALSourceb.Looping, true);
+        }
+
+        private void CheckBox_UnChecked(object sender, RoutedEventArgs e)
+        {
+            AL.Source(_source, ALSourceb.Looping, false);
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            AL.SourcePause(_source);
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            AL.SourceStop(_source);
+        }
+
+        private void Set_Source_Position_Click(object sender, RoutedEventArgs e)
+        {
+            if (!float.TryParse(SourcePositionX.Text.Replace('.',','), out var x) || 
+                !float.TryParse(SourcePositionY.Text.Replace('.', ','), out var y) ||
+                !float.TryParse(SourcePositionZ.Text.Replace('.', ','), out var z))
+                MessageBox.Show("Neispravan unos!");
+            else
+                AL.Source(_source, ALSource3f.Position, x, y, z);
+        }
+
+        private void Set_Source_Velocity_Click(object sender, RoutedEventArgs e)
+        {
+            if (!float.TryParse(SourceVelocityX.Text.Replace('.', ','), out var x) ||
+                !float.TryParse(SourceVelocityY.Text.Replace('.', ','), out var y) ||
+                !float.TryParse(SourceVelocityZ.Text.Replace('.', ','), out var z))
+                MessageBox.Show("Neispravan unos!");
+            else
+                AL.Source(_source, ALSource3f.Velocity, x, y, z);
+        }
+
+        private void Set_Listener_Position_Click(object sender, RoutedEventArgs e)
+        {
+            if (!float.TryParse(ListenerPositionX.Text.Replace('.', ','), out var x) ||
+                !float.TryParse(ListenerPositionY.Text.Replace('.', ','), out var y) ||
+                !float.TryParse(ListenerPositionZ.Text.Replace('.', ','), out var z))
+                MessageBox.Show("Neispravan unos!");
+            else
+                AL.Listener(ALListener3f.Position, x, y, z);
+        }
+
+        private void Set_Listener_Velocity_Click(object sender, RoutedEventArgs e)
+        {
+            if (!float.TryParse(ListenerVelocityX.Text.Replace('.', ','), out var x) ||
+                !float.TryParse(ListenerVelocityY.Text.Replace('.', ','), out var y) ||
+                !float.TryParse(ListenerVelocityZ.Text.Replace('.', ','), out var z))
+                MessageBox.Show("Neispravan unos!");
+            else
+                AL.Listener(ALListener3f.Velocity, x, y, z);
+        }
+
+        private void Set_Source_Gain_Click(object sender, RoutedEventArgs e)
+        {
+            if (!float.TryParse(SourceGain.Text.Replace('.', ','), out var num))
+                MessageBox.Show("Neispravan unos!");
+            else
+                AL.Source(_source, ALSourcef.Gain, num);
+        }
+
+        private void Release_Resources(object sender, EventArgs e)
+        {
+            AL.SourceStop(_source);
+            AL.DeleteSource(_source);
+            AL.DeleteBuffer(_buffer);
+            Alc.MakeContextCurrent(ContextHandle.Zero);
+            _context.Dispose();
+        }
+    }
+}
